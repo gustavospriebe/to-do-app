cf43be4c8f972f4305c177190aaaa9e1d8af4a74
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-01-25 01:11:38 -0300

    responsive and dark mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [darkMode, setDarkMode] = useState(false);
+    const [appTheme, setAppTheme] = useState("App");
+
+    useEffect(() => {
+        darkMode ? setAppTheme("App-dark") : setAppTheme("App");
+    }, [darkMode]);
+
+    const handleChange = () => {
+        setDarkMode(!darkMode);
+    };
+
+    return (
+        <>
+            <div className={appTheme}>
+                <Header handleChange={handleChange} darkMode={darkMode} />
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import moon from "../assets/icon-moon.svg";
+import sun from "../assets/icon-sun.svg";
+import "./Header.css";
+
+export default function Header(props) {
+    const [button, setButton] = useState(moon);
+
+    useEffect(() => {
+        props.darkMode ? setButton(sun) : setButton(moon);
+    }, [props.darkMode]);
+
+    return (
+        <header>
+            <h2>TODO</h2>
+            <img onClick={props.handleChange} src={button} alt="dark mode" />
+        </header>
+    );
+}
